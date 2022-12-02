6c9c927c60c4b3ea35224d7734ff10d24a4e615a
HanHo Choi <64696968+guguhanogu@users.noreply.github.com>
2022-12-02 09:42:55 +0900

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by 최한호 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
